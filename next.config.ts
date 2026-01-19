6eb58c85065663cd0875e2041e3dc61226266beb
shizzles <shizzles18@gmail.com>
2026-01-19 19:20:14 +0800

    .

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+console.log("DEBUG: NEXT_PUBLIC_SUPABASE_URL =", process.env.NEXT_PUBLIC_SUPABASE_URL);
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
